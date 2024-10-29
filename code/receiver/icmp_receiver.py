070a2dee0d48cade9248b700d9536a6cd4bd2d6c
Ahmet Ertaş <ertas8241@gmail.com>
2024-10-29 17:40:43 +0300

    Html rebuild

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,9 +1,13 @@
 from scapy.all import *
 
+# Callback function that will run whenever a packet is received
 def packet_callback(packet):
+    # Check if the packet is ICMP and is a request packet
     if packet.haslayer(ICMP) and packet[IP].ttl == 1:
         print("Received ICMP Request packet:")
+        # Display packet details
         packet.show()
 
 print("Listening for incoming ICMP packets with TTL=1...")
-sniff(filter="icmp", prn=packet_callback, store=0) # wait for incoming packets
+# Wait for incoming packets
+sniff(filter="icmp", prn=packet_callback, store=0)
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,6 +1,10 @@
 from scapy.all import *
 
+# Specify destination hostname
 DESTINATION = "receiver"
+# Create the ICMP packet with TTL=1
 icmp_packet = IP(dst=DESTINATION, ttl=1) / ICMP()
+
 print("Sending ICMP packet with TTL=1 to", DESTINATION)
+# Send the ICMP packet
 send(icmp_packet)
